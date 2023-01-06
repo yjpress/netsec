b437fceb125a61133cb66351b80e34d5667ea392
yjpress <yjpress@hanmail.net>
2023-01-06 13:27:02 +0900

    com2

--- junsec/src/main/java/kr/danbee/junsec/JunsecApplication.java
+++ junsec/src/main/java/kr/danbee/junsec/JunsecApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JunsecApplication {
 
 	public static void main(String[] args) {
-		//git test
+		//git test2
 		SpringApplication.run(JunsecApplication.class, args);
 	}
 
