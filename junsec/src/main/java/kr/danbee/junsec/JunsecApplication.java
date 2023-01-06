88b25a9eb2cff4c0287df1f7cfa7eb42e30739f3
yjpress <yjpress@hanmail.net>
2023-01-06 13:26:00 +0900

    f commit

---
+++ junsec/src/main/java/kr/danbee/junsec/JunsecApplication.java
@@ -0,0 +1,14 @@
+package kr.danbee.junsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunsecApplication {
+
+	public static void main(String[] args) {
+		//git test
+		SpringApplication.run(JunsecApplication.class, args);
+	}
+
+}
---
+++ junsec/src/test/java/kr/danbee/junsec/JunsecApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.danbee.junsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
